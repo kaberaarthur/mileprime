7da7afe27a8778264cacb477f05a6a154dc56971
Arthur Kabera <arthurkabera@gmail.com>
2024-03-12 15:57:14 +0300

    Proceed on Destination not working perfectly

--- lib/view/places_destination/places_destination_screen.dart
+++ lib/view/places_destination/places_destination_screen.dart
@@ -20,6 +20,10 @@ class _PlacesDestinationScreenState extends State<PlacesDestinationScreen> {
   String _sessionToken = '1234567890';
   List<dynamic> _placeList = [];
 
+  final String _selectedDestination = '';
+
+  final Map<String, dynamic> _myDestination = {};
+
   @override
   void initState() {
     super.initState();
@@ -53,13 +57,65 @@ class _PlacesDestinationScreenState extends State<PlacesDestinationScreen> {
       }
       if (response.statusCode == 200) {
         setState(() {
-          _placeList = json.decode(response.body)['predictions'];
+          _placeList = data['predictions'];
         });
       } else {
         throw Exception('Failed to load predictions');
       }
     } catch (e) {
       print(e);
+    }
+  }
+
+  void _updateMyDestination(String placeDescription, double lat, double lng) {
+    setState(() {
+      _myDestination['0'] = {
+        'description': placeDescription,
+        'location': {
+          'lat': lat,
+          'lng': lng,
+        },
+      };
+    });
+  }
+
+  Future<Map<String, double>?> getCoordinates(
+      String placeId, String placeDescription) async {
+    // Replace YOUR_API_KEY with your actual API key
+    const String API_KEY = "AIzaSyD0kPJKSOU4qtXrvddyAZFHeXQY2LMrz_M";
+
+    // Construct the URL for the Geocoding API request
+    String baseURL = 'https://maps.googleapis.com/maps/api/geocode/json';
+    String requestURL = '$baseURL?place_id=$placeId&key=$API_KEY';
+
+    try {
+      // Send the HTTP request
+      var response = await http.get(Uri.parse(requestURL));
+
+      // Check if the request was successful (status code 200)
+      if (response.statusCode == 200) {
+        // Parse the JSON response
+        var data = json.decode(response.body);
+
+        // Extract the latitude and longitude from the response
+        double lat = data['results'][0]['geometry']['location']['lat'];
+        double lng = data['results'][0]['geometry']['location']['lng'];
+
+        _updateMyDestination(placeDescription, lat, lng);
+
+        // Return the latitude and longitude as a map
+        return {
+          'latitude': lat,
+          'longitude': lng,
+        };
+      } else {
+        // If the request was not successful, throw an error
+        throw Exception('Failed to fetch coordinates');
+      }
+    } catch (e) {
+      // If an error occurs during the HTTP request, print the error and return null
+      print(e);
+      return null;
     }
   }
 
@@ -103,23 +159,60 @@ class _PlacesDestinationScreenState extends State<PlacesDestinationScreen> {
                   onTap: () async {
                     debugPrint(
                         "Selected Item: ${_placeList[index]["description"]}");
+                    debugPrint("Latitude: ${_placeList[index]}");
+
+                    Map<String, double>? coordinates = await getCoordinates(
+                        _placeList[index]["place_id"],
+                        _placeList[index]["description"]);
+                    if (coordinates != null) {
+                      debugPrint("Latitude: ${coordinates['latitude']}");
+                      debugPrint("Longitude: ${coordinates['longitude']}");
+
+                      debugPrint(json.encode(_myDestination));
+                    } else {
+                      debugPrint("Failed to fetch coordinates");
+                    }
                   },
                   child: ListTile(
-                    title: Text(_placeList[index]["description"]),
+                    title: Text(
+                      _placeList[index]["description"],
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
                   ),
                 );
               },
             ),
           ),
           SizedBox(
-            height: 16.0,
+            height: 12.0,
           ),
-          ElevatedButton(
-            onPressed: () {
-              Get.to(() => PlacesOriginScreen());
-            },
-            child: Text('Set Origin'),
-          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: _selectedDestination.isEmpty
+                  ? null // Disable button if _selectedDestination is empty
+                  : () {
+                      Get.to(() => PlacesOriginScreen());
+                    },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: _myDestination.isEmpty
+                    ? Colors
+                        .grey // Grey background if _selectedDestination is empty
+                    : Colors.black,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4.0),
+                ),
+              ),
+              child: Text(
+                'Proceed',
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ),
+          )
         ],
       ),
     );
